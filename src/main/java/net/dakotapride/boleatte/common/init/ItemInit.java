8787946ea26b2017c7d0b5bd8a6ef778bbbcd266
DakotaPride <rjtube13@gmail.com>
2022-08-05 22:57:10 -0500

    Boleatte - Added More Foods/Ingredients

--- src/main/java/net/dakotapride/boleatte/common/init/ItemInit.java
+++ src/main/java/net/dakotapride/boleatte/common/init/ItemInit.java
@@ -13,6 +13,11 @@ public class ItemInit {
     public static Item BASOLOTE = new Item(new FabricItemSettings().food(FoodComponentInit.BASOLOTE));
     public static Item RAMUKAI = new Item(new FabricItemSettings().food(FoodComponentInit.RAMUKAI));
     public static Item NIKOTA_TU = new Item(new FabricItemSettings().food(FoodComponentInit.NIKOTA_TU));
+    public static Item MIERIRE = new Item(new FabricItemSettings().food(FoodComponentInit.MIERIRE));
+    public static Item PAIORE = new Item(new FabricItemSettings().food(FoodComponentInit.PAIORE));
+    public static Item PAIEUX = new Item(new FabricItemSettings().food(FoodComponentInit.PAIEUX));
+    public static Item PAIEUX_DOUGH = new Item(new FabricItemSettings());
+    public static Item DE_FEATIANTE = new Item(new FabricItemSettings().food(FoodComponentInit.DE_FEATIANTE));
     public static RamoliteHoneyBottleItem RAMOLITE_HONEY = new RamoliteHoneyBottleItem(new FabricItemSettings().food(FoodComponentInit.RAMOLITE_HONEY));
 
     public static Item PROTOSTERM_BARK = new Item(new FabricItemSettings());
@@ -23,6 +28,11 @@ public class ItemInit {
         Registry.register(Registry.ITEM, new Identifier(ID, "ramukai"), RAMUKAI);
         Registry.register(Registry.ITEM, new Identifier(ID, "ramolite_honey"), RAMOLITE_HONEY);
         Registry.register(Registry.ITEM, new Identifier(ID, "nikota_tu"), NIKOTA_TU);
+        Registry.register(Registry.ITEM, new Identifier(ID, "mierire"), MIERIRE);
+        Registry.register(Registry.ITEM, new Identifier(ID, "paiore"), PAIORE);
+        Registry.register(Registry.ITEM, new Identifier(ID, "paieux"), PAIEUX);
+        Registry.register(Registry.ITEM, new Identifier(ID, "paieux_dough"), PAIEUX_DOUGH);
+        Registry.register(Registry.ITEM, new Identifier(ID, "de_featiante"), DE_FEATIANTE);
 
         Registry.register(Registry.ITEM, new Identifier(ID, "protosterm_bark"), PROTOSTERM_BARK);
     }
