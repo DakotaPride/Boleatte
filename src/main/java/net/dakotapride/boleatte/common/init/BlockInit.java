4895f609cae707bbea1b4d9b35bdc5ac8af52578
DakotaPride <rjtube13@gmail.com>
2022-08-05 22:57:48 -0500

    Boleatte - Added More Protosterm Variants

--- src/main/java/net/dakotapride/boleatte/common/init/BlockInit.java
+++ src/main/java/net/dakotapride/boleatte/common/init/BlockInit.java
@@ -12,11 +12,14 @@ import static net.dakotapride.boleatte.common.BoleatteMain.ID;
 
 public class BlockInit {
 
-    public static SaplingBlock PROTOSTERM_SAPLING = new SaplingBlock(new ProtostermSaplingGenerator(),
-                    FabricBlockSettings.copy(Blocks.OAK_SAPLING).nonOpaque());
+    public static SaplingBlock PROTOSTERM_SAPLING = new SaplingBlock(new ProtostermSaplingGenerator(15),
+                    FabricBlockSettings.copy(Blocks.OAK_SAPLING).ticksRandomly().nonOpaque());
     public static PillarBlock PROTOSTERM_LOG = new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_LOG));
     public static PillarBlock STRIPPED_PROTOSTERM_LOG = new PillarBlock(FabricBlockSettings.copy(Blocks.STRIPPED_OAK_LOG));
+    public static PillarBlock PROTOSTERM_WOOD = new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_WOOD));
+    public static PillarBlock STRIPPED_PROTOSTERM_WOOD = new PillarBlock(FabricBlockSettings.copy(Blocks.STRIPPED_OAK_WOOD));
     public static LeavesBlock PROTOSTERM_LEAVES = new LeavesBlock(FabricBlockSettings.copy(Blocks.OAK_LEAVES).nonOpaque());
+    public static Block PROTOSTERM_PLANKS = new Block(FabricBlockSettings.copy(Blocks.OAK_PLANKS));
 
     public static void init() {
         Registry.register(Registry.BLOCK, new Identifier(ID, "protosterm_sapling"), PROTOSTERM_SAPLING);
@@ -27,6 +30,12 @@ public class BlockInit {
         Registry.register(Registry.ITEM, new Identifier(ID, "protosterm_leaves"), new BlockItem(PROTOSTERM_LEAVES, new FabricItemSettings()));
         Registry.register(Registry.BLOCK, new Identifier(ID, "stripped_protosterm_log"), STRIPPED_PROTOSTERM_LOG);
         Registry.register(Registry.ITEM, new Identifier(ID, "stripped_protosterm_log"), new BlockItem(STRIPPED_PROTOSTERM_LOG, new FabricItemSettings()));
+        Registry.register(Registry.BLOCK, new Identifier(ID, "protosterm_wood"), PROTOSTERM_WOOD);
+        Registry.register(Registry.ITEM, new Identifier(ID, "protosterm_wood"), new BlockItem(PROTOSTERM_WOOD, new FabricItemSettings()));
+        Registry.register(Registry.BLOCK, new Identifier(ID, "stripped_protosterm_wood"), STRIPPED_PROTOSTERM_WOOD);
+        Registry.register(Registry.ITEM, new Identifier(ID, "stripped_protosterm_wood"), new BlockItem(STRIPPED_PROTOSTERM_WOOD, new FabricItemSettings()));
+        Registry.register(Registry.BLOCK, new Identifier(ID, "protosterm_planks"), PROTOSTERM_PLANKS);
+        Registry.register(Registry.ITEM, new Identifier(ID, "protosterm_planks"), new BlockItem(PROTOSTERM_PLANKS, new FabricItemSettings()));
     }
 
 }
