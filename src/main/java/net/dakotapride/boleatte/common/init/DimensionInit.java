bcca27f0f35a975cc8c3c61a94de0ee75da27b17
DakotaPride <rjtube13@gmail.com>
2022-08-08 15:52:04 -0500

    Boleatte - Updated Protosterm Forest And Quantila Barrens Biomes

--- src/main/java/net/dakotapride/boleatte/common/init/DimensionInit.java
+++ src/main/java/net/dakotapride/boleatte/common/init/DimensionInit.java
@@ -2,6 +2,7 @@ package net.dakotapride.boleatte.common.init;
 
 import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
 import net.minecraft.block.Blocks;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
@@ -10,15 +11,15 @@ import net.minecraft.world.World;
 import static net.dakotapride.boleatte.common.BoleatteMain.ID;
 
 public class DimensionInit {
-    public static final RegistryKey<World> DEPIEN_DIMENSION_KEY = RegistryKey.of(Registry.WORLD_KEY,
-            new Identifier(ID, "depien"));
+    public static final RegistryKey<World> BOLEATTE_DIMENSION_KEY = RegistryKey.of(Registry.WORLD_KEY,
+            new Identifier(ID, "boleatte"));
 
     public static void init() {
         CustomPortalBuilder.beginPortal()
                 .frameBlock(Blocks.WARPED_WART_BLOCK)
-                .destDimID(DEPIEN_DIMENSION_KEY.getValue())
+                .destDimID(BOLEATTE_DIMENSION_KEY.getValue())
                 .tintColor(45, 79, 195)
-                .lightWithItem(ItemInit.PROTOSTERM_SHIELD_BASE)
+                .lightWithItem(Items.DIAMOND)
                 .onlyLightInOverworld()
                 .registerPortal();
     }
