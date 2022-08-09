e8b07d3fb248307389b35976ecb2d4d19f3199bd
DakotaPride <rjtube13@gmail.com>
2022-08-09 14:01:33 -0500

    Boleatte - Preparing For Beta Release

--- src/main/java/net/dakotapride/boleatte/common/init/BlockInit.java
+++ src/main/java/net/dakotapride/boleatte/common/init/BlockInit.java
@@ -32,7 +32,6 @@ public class BlockInit {
     public static PillarBlock STRIPPED_BOTAKOA_WOOD = new PillarBlock(FabricBlockSettings.copy(Blocks.STRIPPED_OAK_WOOD));
     public static Block BOTAKOA_PLANKS = new Block(FabricBlockSettings.copy(Blocks.OAK_PLANKS));
     public static LeavesBlock BOTAKOA_LEAVES = new LeavesBlock(FabricBlockSettings.copy(Blocks.OAK_LEAVES).ticksRandomly().nonOpaque());
-    public static BarrelBlock BOTAKOA_BARREL = new BarrelBlock(FabricBlockSettings.copy(Blocks.BARREL));
 
     public static SaplingBlock ARBUNE_SAPLING = new SaplingBlock(new ArbuneSaplingGenerator(),
             FabricBlockSettings.copy(Blocks.OAK_SAPLING).ticksRandomly().nonOpaque());
@@ -48,51 +47,49 @@ public class BlockInit {
 
     public static void init() {
         Registry.register(Registry.BLOCK, new Identifier(ID, "protosterm_sapling"), PROTOSTERM_SAPLING);
-        Registry.register(Registry.ITEM, new Identifier(ID, "protosterm_sapling"), new BlockItem(PROTOSTERM_SAPLING, new FabricItemSettings()));
+        Registry.register(Registry.ITEM, new Identifier(ID, "protosterm_sapling"), new BlockItem(PROTOSTERM_SAPLING, new FabricItemSettings().group(ItemInit.BoleatteItemGroup.BOLEATTE)));
         Registry.register(Registry.BLOCK, new Identifier(ID, "protosterm_log"), PROTOSTERM_LOG);
-        Registry.register(Registry.ITEM, new Identifier(ID, "protosterm_log"), new BlockItem(PROTOSTERM_LOG, new FabricItemSettings()));
+        Registry.register(Registry.ITEM, new Identifier(ID, "protosterm_log"), new BlockItem(PROTOSTERM_LOG, new FabricItemSettings().group(ItemInit.BoleatteItemGroup.BOLEATTE)));
         Registry.register(Registry.BLOCK, new Identifier(ID, "protosterm_leaves"), PROTOSTERM_LEAVES);
-        Registry.register(Registry.ITEM, new Identifier(ID, "protosterm_leaves"), new BlockItem(PROTOSTERM_LEAVES, new FabricItemSettings()));
+        Registry.register(Registry.ITEM, new Identifier(ID, "protosterm_leaves"), new BlockItem(PROTOSTERM_LEAVES, new FabricItemSettings().group(ItemInit.BoleatteItemGroup.BOLEATTE)));
         Registry.register(Registry.BLOCK, new Identifier(ID, "stripped_protosterm_log"), STRIPPED_PROTOSTERM_LOG);
-        Registry.register(Registry.ITEM, new Identifier(ID, "stripped_protosterm_log"), new BlockItem(STRIPPED_PROTOSTERM_LOG, new FabricItemSettings()));
+        Registry.register(Registry.ITEM, new Identifier(ID, "stripped_protosterm_log"), new BlockItem(STRIPPED_PROTOSTERM_LOG, new FabricItemSettings().group(ItemInit.BoleatteItemGroup.BOLEATTE)));
         Registry.register(Registry.BLOCK, new Identifier(ID, "protosterm_wood"), PROTOSTERM_WOOD);
-        Registry.register(Registry.ITEM, new Identifier(ID, "protosterm_wood"), new BlockItem(PROTOSTERM_WOOD, new FabricItemSettings()));
+        Registry.register(Registry.ITEM, new Identifier(ID, "protosterm_wood"), new BlockItem(PROTOSTERM_WOOD, new FabricItemSettings().group(ItemInit.BoleatteItemGroup.BOLEATTE)));
         Registry.register(Registry.BLOCK, new Identifier(ID, "stripped_protosterm_wood"), STRIPPED_PROTOSTERM_WOOD);
-        Registry.register(Registry.ITEM, new Identifier(ID, "stripped_protosterm_wood"), new BlockItem(STRIPPED_PROTOSTERM_WOOD, new FabricItemSettings()));
+        Registry.register(Registry.ITEM, new Identifier(ID, "stripped_protosterm_wood"), new BlockItem(STRIPPED_PROTOSTERM_WOOD, new FabricItemSettings().group(ItemInit.BoleatteItemGroup.BOLEATTE)));
         Registry.register(Registry.BLOCK, new Identifier(ID, "protosterm_planks"), PROTOSTERM_PLANKS);
-        Registry.register(Registry.ITEM, new Identifier(ID, "protosterm_planks"), new BlockItem(PROTOSTERM_PLANKS, new FabricItemSettings()));
+        Registry.register(Registry.ITEM, new Identifier(ID, "protosterm_planks"), new BlockItem(PROTOSTERM_PLANKS, new FabricItemSettings().group(ItemInit.BoleatteItemGroup.BOLEATTE)));
 
         Registry.register(Registry.BLOCK, new Identifier(ID, "botakoa_sapling"), BOTAKOA_SAPLING);
-        Registry.register(Registry.ITEM, new Identifier(ID, "botakoa_sapling"), new BlockItem(BOTAKOA_SAPLING, new FabricItemSettings()));
+        Registry.register(Registry.ITEM, new Identifier(ID, "botakoa_sapling"), new BlockItem(BOTAKOA_SAPLING, new FabricItemSettings().group(ItemInit.BoleatteItemGroup.BOLEATTE)));
         Registry.register(Registry.BLOCK, new Identifier(ID, "botakoa_log"), BOTAKOA_LOG);
-        Registry.register(Registry.ITEM, new Identifier(ID, "botakoa_log"), new BlockItem(BOTAKOA_LOG, new FabricItemSettings()));
+        Registry.register(Registry.ITEM, new Identifier(ID, "botakoa_log"), new BlockItem(BOTAKOA_LOG, new FabricItemSettings().group(ItemInit.BoleatteItemGroup.BOLEATTE)));
         Registry.register(Registry.BLOCK, new Identifier(ID, "botakoa_leaves"), BOTAKOA_LEAVES);
-        Registry.register(Registry.ITEM, new Identifier(ID, "botakoa_leaves"), new BlockItem(BOTAKOA_LEAVES, new FabricItemSettings()));
+        Registry.register(Registry.ITEM, new Identifier(ID, "botakoa_leaves"), new BlockItem(BOTAKOA_LEAVES, new FabricItemSettings().group(ItemInit.BoleatteItemGroup.BOLEATTE)));
         Registry.register(Registry.BLOCK, new Identifier(ID, "stripped_botakoa_log"), STRIPPED_BOTAKOA_LOG);
-        Registry.register(Registry.ITEM, new Identifier(ID, "stripped_botakoa_log"), new BlockItem(STRIPPED_BOTAKOA_LOG, new FabricItemSettings()));
+        Registry.register(Registry.ITEM, new Identifier(ID, "stripped_botakoa_log"), new BlockItem(STRIPPED_BOTAKOA_LOG, new FabricItemSettings().group(ItemInit.BoleatteItemGroup.BOLEATTE)));
         Registry.register(Registry.BLOCK, new Identifier(ID, "botakoa_wood"), BOTAKOA_WOOD);
-        Registry.register(Registry.ITEM, new Identifier(ID, "botakoa_wood"), new BlockItem(BOTAKOA_WOOD, new FabricItemSettings()));
+        Registry.register(Registry.ITEM, new Identifier(ID, "botakoa_wood"), new BlockItem(BOTAKOA_WOOD, new FabricItemSettings().group(ItemInit.BoleatteItemGroup.BOLEATTE)));
         Registry.register(Registry.BLOCK, new Identifier(ID, "stripped_botakoa_wood"), STRIPPED_BOTAKOA_WOOD);
-        Registry.register(Registry.ITEM, new Identifier(ID, "stripped_botakoa_wood"), new BlockItem(STRIPPED_BOTAKOA_WOOD, new FabricItemSettings()));
+        Registry.register(Registry.ITEM, new Identifier(ID, "stripped_botakoa_wood"), new BlockItem(STRIPPED_BOTAKOA_WOOD, new FabricItemSettings().group(ItemInit.BoleatteItemGroup.BOLEATTE)));
         Registry.register(Registry.BLOCK, new Identifier(ID, "botakoa_planks"), BOTAKOA_PLANKS);
-        Registry.register(Registry.ITEM, new Identifier(ID, "botakoa_planks"), new BlockItem(BOTAKOA_PLANKS, new FabricItemSettings()));
-        Registry.register(Registry.BLOCK, new Identifier(ID, "botakoa_barrel"), BOTAKOA_BARREL);
-        Registry.register(Registry.ITEM, new Identifier(ID, "botakoa_barrel"), new BlockItem(BOTAKOA_BARREL, new FabricItemSettings()));
+        Registry.register(Registry.ITEM, new Identifier(ID, "botakoa_planks"), new BlockItem(BOTAKOA_PLANKS, new FabricItemSettings().group(ItemInit.BoleatteItemGroup.BOLEATTE)));
 
         Registry.register(Registry.BLOCK, new Identifier(ID, "arbune_sapling"), ARBUNE_SAPLING);
-        Registry.register(Registry.ITEM, new Identifier(ID, "arbune_sapling"), new BlockItem(ARBUNE_SAPLING, new FabricItemSettings()));
+        Registry.register(Registry.ITEM, new Identifier(ID, "arbune_sapling"), new BlockItem(ARBUNE_SAPLING, new FabricItemSettings().group(ItemInit.BoleatteItemGroup.BOLEATTE)));
         Registry.register(Registry.BLOCK, new Identifier(ID, "arbune_log"), ARBUNE_LOG);
-        Registry.register(Registry.ITEM, new Identifier(ID, "arbune_log"), new BlockItem(ARBUNE_LOG, new FabricItemSettings()));
+        Registry.register(Registry.ITEM, new Identifier(ID, "arbune_log"), new BlockItem(ARBUNE_LOG, new FabricItemSettings().group(ItemInit.BoleatteItemGroup.BOLEATTE)));
         Registry.register(Registry.BLOCK, new Identifier(ID, "arbune_leaves"), ARBUNE_LEAVES);
-        Registry.register(Registry.ITEM, new Identifier(ID, "arbune_leaves"), new BlockItem(ARBUNE_LEAVES, new FabricItemSettings()));
+        Registry.register(Registry.ITEM, new Identifier(ID, "arbune_leaves"), new BlockItem(ARBUNE_LEAVES, new FabricItemSettings().group(ItemInit.BoleatteItemGroup.BOLEATTE)));
         Registry.register(Registry.BLOCK, new Identifier(ID, "stripped_arbune_log"), STRIPPED_ARBUNE_LOG);
-        Registry.register(Registry.ITEM, new Identifier(ID, "stripped_arbune_log"), new BlockItem(STRIPPED_ARBUNE_LOG, new FabricItemSettings()));
+        Registry.register(Registry.ITEM, new Identifier(ID, "stripped_arbune_log"), new BlockItem(STRIPPED_ARBUNE_LOG, new FabricItemSettings().group(ItemInit.BoleatteItemGroup.BOLEATTE)));
         Registry.register(Registry.BLOCK, new Identifier(ID, "arbune_wood"), ARBUNE_WOOD);
-        Registry.register(Registry.ITEM, new Identifier(ID, "arbune_wood"), new BlockItem(ARBUNE_WOOD, new FabricItemSettings()));
+        Registry.register(Registry.ITEM, new Identifier(ID, "arbune_wood"), new BlockItem(ARBUNE_WOOD, new FabricItemSettings().group(ItemInit.BoleatteItemGroup.BOLEATTE)));
         Registry.register(Registry.BLOCK, new Identifier(ID, "stripped_arbune_wood"), STRIPPED_ARBUNE_WOOD);
-        Registry.register(Registry.ITEM, new Identifier(ID, "stripped_arbune_wood"), new BlockItem(STRIPPED_ARBUNE_WOOD, new FabricItemSettings()));
+        Registry.register(Registry.ITEM, new Identifier(ID, "stripped_arbune_wood"), new BlockItem(STRIPPED_ARBUNE_WOOD, new FabricItemSettings().group(ItemInit.BoleatteItemGroup.BOLEATTE)));
         Registry.register(Registry.BLOCK, new Identifier(ID, "arbune_planks"), ARBUNE_PLANKS);
-        Registry.register(Registry.ITEM, new Identifier(ID, "arbune_planks"), new BlockItem(ARBUNE_PLANKS, new FabricItemSettings()));
+        Registry.register(Registry.ITEM, new Identifier(ID, "arbune_planks"), new BlockItem(ARBUNE_PLANKS, new FabricItemSettings().group(ItemInit.BoleatteItemGroup.BOLEATTE)));
 
         Registry.register(Registry.BLOCK, new Identifier(ID, "quanalla_bush"), QUANALLA_BUSH);
     }
--- src/main/java/net/dakotapride/boleatte/common/init/ItemInit.java
+++ src/main/java/net/dakotapride/boleatte/common/init/ItemInit.java
@@ -1,32 +1,74 @@
 package net.dakotapride.boleatte.common.init;
 
 import net.dakotapride.boleatte.common.item.RamoliteHoneyBottleItem;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 import static net.dakotapride.boleatte.common.BoleatteMain.ID;
 
 public class ItemInit {
-    public static BlockItem QUANALLA = new BlockItem(BlockInit.QUANALLA_BUSH, new FabricItemSettings().food(FoodComponentInit.QUANALLA));
-    public static Item BASOLOTE = new Item(new FabricItemSettings().food(FoodComponentInit.BASOLOTE));
-    public static Item RAMUKAI = new Item(new FabricItemSettings().food(FoodComponentInit.RAMUKAI));
-    public static Item NIKOTA_TU = new Item(new FabricItemSettings().food(FoodComponentInit.NIKOTA_TU));
-    public static Item MIERIRE = new Item(new FabricItemSettings().food(FoodComponentInit.MIERIRE));
-    public static Item PAIORE = new Item(new FabricItemSettings().food(FoodComponentInit.PAIORE));
-    public static Item PAIEUX = new Item(new FabricItemSettings().food(FoodComponentInit.PAIEUX));
-    public static Item PAIEUX_DOUGH = new Item(new FabricItemSettings());
-    public static Item DE_FEATIANTE = new Item(new FabricItemSettings().food(FoodComponentInit.DE_FEATIANTE));
-    public static RamoliteHoneyBottleItem RAMOLITE_HONEY = new RamoliteHoneyBottleItem(new FabricItemSettings().food(FoodComponentInit.RAMOLITE_HONEY));
-    public static Item ALPHIAGOU = new Item(new FabricItemSettings().food(FoodComponentInit.ALPHIAGOU));
-    public static Item ALPHIAGOU_CIDER = new Item(new FabricItemSettings().food(FoodComponentInit.ALPHIAGOU_CIDER));
-    public static Item MYSTICAL_ALPHIAGOU_CIDER = new Item(new FabricItemSettings().food(FoodComponentInit.MYSTICAL_ALPHIAGOU_CIDER));
 
-    public static Item PROTOSTERM_BARK = new Item(new FabricItemSettings());
+    public static class BoleatteItemGroup {
+        public static ItemGroup BOLEATTE = FabricItemGroupBuilder.create(
+                        new Identifier(ID, "boleatte_group"))
+                .icon(() -> new ItemStack(Items.SPLASH_POTION))
+                .appendItems(itemStacks -> {
+                    itemStacks.add(new ItemStack(QUANALLA));
+                    itemStacks.add(new ItemStack(BASOLOTE));
+                    itemStacks.add(new ItemStack(RAMUKAI));
+                    itemStacks.add(new ItemStack(RAMOLITE_HONEY));
+                    itemStacks.add(new ItemStack(NIKOTA_TU));
+                    itemStacks.add(new ItemStack(MIERIRE));
+                    itemStacks.add(new ItemStack(PAIORE));
+                    itemStacks.add(new ItemStack(PAIEUX_DOUGH));
+                    itemStacks.add(new ItemStack(PAIEUX));
+                    itemStacks.add(new ItemStack(ALPHIAGOU));
+                    itemStacks.add(new ItemStack(DE_FEATIANTE));
+                    itemStacks.add(new ItemStack(ALPHIAGOU_CIDER));
+                    itemStacks.add(new ItemStack(MYSTICAL_ALPHIAGOU_CIDER));
 
+                    itemStacks.add(new ItemStack(BlockInit.PROTOSTERM_LOG.asItem()));
+                    itemStacks.add(new ItemStack(BlockInit.PROTOSTERM_WOOD.asItem()));
+                    itemStacks.add(new ItemStack(BlockInit.STRIPPED_PROTOSTERM_LOG.asItem()));
+                    itemStacks.add(new ItemStack(BlockInit.STRIPPED_PROTOSTERM_WOOD.asItem()));
+                    itemStacks.add(new ItemStack(BlockInit.PROTOSTERM_PLANKS.asItem()));
+                    itemStacks.add(new ItemStack(BlockInit.PROTOSTERM_LEAVES.asItem()));
+                    itemStacks.add(new ItemStack(BlockInit.PROTOSTERM_SAPLING.asItem()));
 
+                    itemStacks.add(new ItemStack(BlockInit.BOTAKOA_LOG.asItem()));
+                    itemStacks.add(new ItemStack(BlockInit.BOTAKOA_WOOD.asItem()));
+                    itemStacks.add(new ItemStack(BlockInit.STRIPPED_BOTAKOA_LOG.asItem()));
+                    itemStacks.add(new ItemStack(BlockInit.STRIPPED_BOTAKOA_WOOD.asItem()));
+                    itemStacks.add(new ItemStack(BlockInit.BOTAKOA_PLANKS.asItem()));
+                    itemStacks.add(new ItemStack(BlockInit.BOTAKOA_LEAVES.asItem()));
+                    itemStacks.add(new ItemStack(BlockInit.BOTAKOA_SAPLING.asItem()));
+
+                    itemStacks.add(new ItemStack(BlockInit.ARBUNE_LOG.asItem()));
+                    itemStacks.add(new ItemStack(BlockInit.ARBUNE_WOOD.asItem()));
+                    itemStacks.add(new ItemStack(BlockInit.STRIPPED_ARBUNE_LOG.asItem()));
+                    itemStacks.add(new ItemStack(BlockInit.STRIPPED_ARBUNE_WOOD.asItem()));
+                    itemStacks.add(new ItemStack(BlockInit.ARBUNE_PLANKS.asItem()));
+                    itemStacks.add(new ItemStack(BlockInit.ARBUNE_LEAVES.asItem()));
+                    itemStacks.add(new ItemStack(BlockInit.ARBUNE_SAPLING.asItem()));
+                }).build();
+    }
+
+    public static BlockItem QUANALLA = new BlockItem(BlockInit.QUANALLA_BUSH, new FabricItemSettings().food(FoodComponentInit.QUANALLA).group(BoleatteItemGroup.BOLEATTE));
+    public static Item BASOLOTE = new Item(new FabricItemSettings().food(FoodComponentInit.BASOLOTE).group(BoleatteItemGroup.BOLEATTE));
+    public static Item RAMUKAI = new Item(new FabricItemSettings().food(FoodComponentInit.RAMUKAI).group(BoleatteItemGroup.BOLEATTE));
+    public static Item NIKOTA_TU = new Item(new FabricItemSettings().food(FoodComponentInit.NIKOTA_TU).group(BoleatteItemGroup.BOLEATTE));
+    public static Item MIERIRE = new Item(new FabricItemSettings().food(FoodComponentInit.MIERIRE).group(BoleatteItemGroup.BOLEATTE));
+    public static Item PAIORE = new Item(new FabricItemSettings().food(FoodComponentInit.PAIORE).group(BoleatteItemGroup.BOLEATTE));
+    public static Item PAIEUX = new Item(new FabricItemSettings().food(FoodComponentInit.PAIEUX).group(BoleatteItemGroup.BOLEATTE));
+    public static Item PAIEUX_DOUGH = new Item(new FabricItemSettings().group(BoleatteItemGroup.BOLEATTE));
+    public static Item DE_FEATIANTE = new Item(new FabricItemSettings().food(FoodComponentInit.DE_FEATIANTE).group(BoleatteItemGroup.BOLEATTE));
+    public static RamoliteHoneyBottleItem RAMOLITE_HONEY = new RamoliteHoneyBottleItem(new FabricItemSettings().food(FoodComponentInit.RAMOLITE_HONEY).group(BoleatteItemGroup.BOLEATTE));
+    public static Item ALPHIAGOU = new Item(new FabricItemSettings().food(FoodComponentInit.ALPHIAGOU).group(BoleatteItemGroup.BOLEATTE));
+    public static Item ALPHIAGOU_CIDER = new Item(new FabricItemSettings().food(FoodComponentInit.ALPHIAGOU_CIDER).group(BoleatteItemGroup.BOLEATTE));
+    public static Item MYSTICAL_ALPHIAGOU_CIDER = new Item(new FabricItemSettings().food(FoodComponentInit.MYSTICAL_ALPHIAGOU_CIDER).group(BoleatteItemGroup.BOLEATTE));
 
     public static void init() {
         Registry.register(Registry.ITEM, new Identifier(ID, "quanalla"), QUANALLA);
@@ -42,7 +84,5 @@ public class ItemInit {
         Registry.register(Registry.ITEM, new Identifier(ID, "alphiagou"), ALPHIAGOU);
         Registry.register(Registry.ITEM, new Identifier(ID, "alphiagou_cider"), ALPHIAGOU_CIDER);
         Registry.register(Registry.ITEM, new Identifier(ID, "mystical_alphiagou_cider"), MYSTICAL_ALPHIAGOU_CIDER);
-
-        Registry.register(Registry.ITEM, new Identifier(ID, "protosterm_bark"), PROTOSTERM_BARK);
     }
 }
