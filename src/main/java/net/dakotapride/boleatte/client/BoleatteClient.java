41bbe2520ecaa94aef8544e7a8afbac42abb6149
DakotaPride <rjtube13@gmail.com>
2022-08-09 14:52:00 -0500

    Boleatte - Preparing For Beta Release

--- src/main/java/net/dakotapride/boleatte/client/BoleatteClient.java
+++ src/main/java/net/dakotapride/boleatte/client/BoleatteClient.java
@@ -14,7 +14,6 @@ public class BoleatteClient implements ClientModInitializer {
 
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.BOTAKOA_SAPLING, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.BOTAKOA_LEAVES, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.BOTAKOA_BARREL, RenderLayer.getCutout());
 
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.ARBUNE_SAPLING, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(BlockInit.ARBUNE_LEAVES, RenderLayer.getCutout());
