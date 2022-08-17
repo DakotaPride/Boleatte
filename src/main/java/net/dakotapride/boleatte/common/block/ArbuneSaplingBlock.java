fb9c0a1ee8e6ba7c47a812c55ffa1089abfc638c
DakotaPride <rjtube13@gmail.com>
2022-08-17 11:28:16 -0500

    Boleatte - Polishing Rementio/Biome Generation

--- src/main/java/net/dakotapride/boleatte/common/block/ArbuneSaplingBlock.java
+++ src/main/java/net/dakotapride/boleatte/common/block/ArbuneSaplingBlock.java
@@ -14,6 +14,6 @@ public class ArbuneSaplingBlock extends SaplingBlock {
 
     @Override
     protected boolean canPlantOnTop(BlockState floor, BlockView world, BlockPos pos) {
-        return floor.isOf(BlockInit.ARBUNE_REMENTIO);
+        return (floor.isOf(BlockInit.ARBUNE_REMENTIO) || floor.isOf(BlockInit.REMENTIO));
     }
 }
