50a244b3d4e89b89f3529f632fbeda1cae37ca0a
DakotaPride <rjtube13@gmail.com>
2022-08-22 11:41:59 -0500

    Boleatte - 0.1.2-beta

--- src/main/java/net/dakotapride/boleatte/common/item/alphiagou/PerfervidAlphiagouItem.java
+++ src/main/java/net/dakotapride/boleatte/common/item/alphiagou/PerfervidAlphiagouItem.java
@@ -1,11 +1,18 @@
 package net.dakotapride.boleatte.common.item.alphiagou;
 
+import com.google.common.collect.Maps;
 import net.dakotapride.boleatte.common.init.TagInit;
 import net.dakotapride.boleatte.common.item.AscunauticItem;
+import net.minecraft.advancement.Advancement;
+import net.minecraft.advancement.AdvancementCriterion;
+import net.minecraft.advancement.criterion.CriterionConditions;
+import net.minecraft.advancement.criterion.TickCriterion;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsage;
+import net.minecraft.predicate.entity.EntityPredicate;
+import net.minecraft.predicate.entity.LocationPredicate;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
@@ -13,6 +20,9 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
+import net.minecraft.world.gen.structure.StructureKeys;
+
+import java.util.Map;
 
 public class PerfervidAlphiagouItem extends AscunauticItem {
     public PerfervidAlphiagouItem(Settings settings) {
@@ -32,7 +42,9 @@ public class PerfervidAlphiagouItem extends AscunauticItem {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         if (!user.getWorld().isClient()) {
-            if (!user.getSteppingBlockState().isIn(TagInit.IS_STERRES_DUNGEON_BLOCKS)) {
+            if (!user.getSteppingBlockState().isIn(TagInit.IS_STERRES_DUNGEON_BLOCKS)
+                    && Advancement.Builder.criterion("ancient_city", TickCriterion.Conditions.createLocation
+                    (LocationPredicate.feature(StructureKeys.ANCIENT_CITY)))) {
                 user.sendMessage(Text.translatable("text.sterres.denial"), false);
                 user.getItemCooldownManager().set(this, 100);
                 return TypedActionResult.fail(user.getStackInHand(hand));
