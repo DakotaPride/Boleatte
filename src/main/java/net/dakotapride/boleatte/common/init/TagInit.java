f6dc0debbfed25138103052fb9a694823d2aefe1
DakotaPride <rjtube13@gmail.com>
2022-09-02 22:52:58 -0500

    Boleatte - Implementing New Biomes + Features

--- src/main/java/net/dakotapride/boleatte/common/init/TagInit.java
+++ src/main/java/net/dakotapride/boleatte/common/init/TagInit.java
@@ -17,6 +17,8 @@ public class TagInit {
     public static final TagKey<Biome> IS_ARBUNE_FOREST = TagKey.of(Registry.BIOME_KEY, new Identifier(ID, "is_arbune_forest"));
     public static final TagKey<Biome> IS_RASIORE_FOREST = TagKey.of(Registry.BIOME_KEY, new Identifier(ID, "is_rasiore_forest"));
     public static final TagKey<Biome> IS_BABURBEN_WASTES = TagKey.of(Registry.BIOME_KEY, new Identifier(ID, "is_baburben_wastes"));
+    public static final TagKey<Biome> IS_ETTERAVE_FOREST = TagKey.of(Registry.BIOME_KEY, new Identifier(ID, "is_etterave_forest"));
+    public static final TagKey<Biome> IS_ARBUNE_PLAINS = TagKey.of(Registry.BIOME_KEY, new Identifier(ID, "is_arbune_plains"));
 
     public static final TagKey<Item> ACCEPTABLE_DIMATIS = TagKey.of(Registry.ITEM_KEY, new Identifier(ID, "acceptable_dimatis"));
 
