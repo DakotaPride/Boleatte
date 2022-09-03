be474d6be358e52c622d3188faa7f247dea5bdfe
DakotaPride <rjtube13@gmail.com>
2022-09-02 21:42:22 -0500

    Boleatte - Implementing New Biomes + Features

--- src/main/java/net/dakotapride/boleatte/common/block/EtteraveBambooBlock.java
+++ src/main/java/net/dakotapride/boleatte/common/block/EtteraveBambooBlock.java
@@ -4,13 +4,9 @@ import net.dakotapride.boleatte.common.init.BlockInit;
 import net.minecraft.block.*;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.item.ItemPlacementContext;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
-import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
 import org.jetbrains.annotations.Nullable;
 
@@ -43,23 +39,6 @@ public class EtteraveBambooBlock extends BambooBlock implements Fertilizable {
     }
 
     @Override
-    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
-        if (!state.canPlaceAt(world, pos)) {
-            world.createAndScheduleBlockTick(pos, this, 1);
-        }
-
-        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
-    }
-
-    @Override
-    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
-        if (!state.canPlaceAt(world, pos)) {
-            world.breakBlock(pos, true);
-        }
-
-    }
-
-    @Override
     @Nullable
     public BlockState getPlacementState(ItemPlacementContext ctx) {
         FluidState fluidState = ctx.getWorld().getFluidState(ctx.getBlockPos());
@@ -79,29 +58,6 @@ public class EtteraveBambooBlock extends BambooBlock implements Fertilizable {
                 }
             } else {
                 return null;
-            }
-        }
-    }
-
-    @Override
-    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
-        BlockPos blockPos = pos.up();
-        if (world.isAir(blockPos)) {
-            int i;
-            for(i = 1; world.getBlockState(pos.down(i)).isOf(this); ++i) {
-            }
-
-            if (i < 3) {
-                int j = (Integer)state.get(AGE);
-                if (j == 15) {
-                    world.setBlockState(blockPos, this.getDefaultState());
-                    BlockState blockState = (BlockState)state.with(AGE, 0);
-                    world.setBlockState(pos, blockState, 4);
-                    world.updateNeighbor(blockState, blockPos, this, pos, false);
-                } else {
-                    world.setBlockState(pos, (BlockState)state.with(AGE, j + 1), 4);
-                }
-
             }
         }
     }
