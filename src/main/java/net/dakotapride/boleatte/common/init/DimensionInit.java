df35ff29d5075c2fcd728354f4bd1f6e6d9d6201
DakotaPride <rjtube13@gmail.com>
2022-11-07 07:41:31 -0600

    Boleatte - Hopefully Fixed Boleatte Dimension (Quilt) Crash v0.2.2-dev

--- src/main/java/net/dakotapride/boleatte/common/init/DimensionInit.java
+++ src/main/java/net/dakotapride/boleatte/common/init/DimensionInit.java
@@ -1,24 +1,19 @@
 package net.dakotapride.boleatte.common.init;
 
 import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
+import net.kyrptonaught.customportalapi.portal.PortalIgnitionSource;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryKey;
-import net.minecraft.world.World;
 
 import static net.dakotapride.boleatte.common.BoleatteMain.ID;
 
 public class DimensionInit {
-    public static final RegistryKey<World> BOLEATTE_DIMENSION_KEY = RegistryKey.of(Registry.WORLD_KEY,
-            new Identifier(ID, "boleatte"));
 
     public static void init() {
         CustomPortalBuilder.beginPortal()
                 .frameBlock(BlockInit.ORETESSE)
-                .lightWithItem(ItemInit.DIMATIS)
-                .destDimID(BOLEATTE_DIMENSION_KEY.getValue())
+                .customIgnitionSource(PortalIgnitionSource.ItemUseSource(ItemInit.DIMATIS.getDefaultStack().getItem()))
+                .destDimID(new Identifier(ID, "boleatte"))
                 .tintColor(211, 255, 239)
-                .onlyLightInOverworld()
                 .registerPortal();
     }
 
